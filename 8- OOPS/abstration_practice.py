57229045c3fb210460d89432b68bfec4417a66c3
6102-siddharth <siddharth.vadgaonkar06@gmail.com>
2026-01-30 16:17:00 +0530

    Added Bank example file

---
+++ 8- OOPS/abstration_practice.py
@@ -0,0 +1,28 @@
+#Questions
+#Create a account class with 2 attributes - balance and account number 
+#Create methods for debit , credit and printing the balance
+
+class Account():
+    def __init__(self,bal,acc):
+        self.balance=bal
+        self.account_no=acc
+    
+    def credit(self,amount):
+        self.balance += amount
+        print("Rs.",amount,"Credited amount")
+        print("Total Balance = ",self.get_balance())
+
+    def debit(self,amount):
+        self.balance -= amount
+        print("Rs.",amount,"debited amount")
+        print("Total Balance =",self.get_balance())
+
+    def get_balance(self):
+        return self.balance
+
+acc1=Account(10000,9087564312)
+# print("Account no",acc1.account_no)
+# print("Total balance",acc1.balance)
+acc1.debit(1000)
+
+acc1.credit(500)
