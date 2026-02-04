9f6da764e49862c70669c2795af36956e0cfaf8b
6102-siddharth <siddharth.vadgaonkar06@gmail.com>
2026-02-04 18:36:16 +0530

    Added Dictinary and Set

---
+++ 4-Dictionary and Sets/dictinary_sets.py
@@ -0,0 +1,18 @@
+info={
+    "name":"Siddharth",
+    "age":22,
+    "Education":"Bachlor's Of Technology",
+    "College_CGPA":"7.2"
+}
+print(info)
+print("------------------------------------------")
+
+info1={
+    "name":"Siddharth",
+    "CGPA":7.2,
+    "marks":"[90,88,67,77,56]",
+    "subjects":"(mat,sci,ss,bio,chem)" ,
+    12:111,
+    7.2:"College"
+}
+print(info1)
