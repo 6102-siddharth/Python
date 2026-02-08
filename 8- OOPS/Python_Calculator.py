0c8bb75cbbdc3d7578437ae058040fa6fda2471a
6102-siddharth <siddharth.vadgaonkar06@gmail.com>
2026-02-08 19:31:15 +0530

    code added by functions

--- 8- OOPS/Python_Calculator.py
+++ 8- OOPS/Python_Calculator.py
@@ -1,17 +1,22 @@
 a=int(input("Enter First Number: "))
 b=int(input("Enter Second number: "))
+def Calculator(a,b):
 
-c=a+b
-print("Addition: ",c)
+    c=a+b  
+    print("Addition: ",c)
 
-d=a-b
-print("Subtraction: ",d)
+    d=a-b
+    print("Subtraction: ",d)
 
-e=a*b
-print("Multiplication:",e)
+    e=a*b
+    print("Multiplication:",e)
 
-f=a/b
-print("Division: ",f)
+    f=a/b
+    print("Division: ",f)
 
-g=a%b
-print("Modulus:",g)
+    g=a%b
+    print("Modulus:",g)
+
+
+Calculator(a,b)
+print(Calculator)
