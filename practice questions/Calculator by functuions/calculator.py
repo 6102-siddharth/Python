f937d1099be83ca7d2f447fe941c09152db3493e
6102-siddharth <siddharth.vadgaonkar06@gmail.com>
2026-02-11 18:32:09 +0530

    Modified Function file

--- practice questions/Calculator by functuions/calculator.py
+++ practice questions/Calculator by functuions/calculator.py
@@ -1,13 +1,18 @@
 def Calculator(a,b,opp):
     if ( opp =="+"):
-        return a+b
+        return "Addition=",a+b
     elif( opp =="-"):
-        return a-b
+        return "Substraction=",a-b
     elif(opp =="*"):
-        return a*b
+        return "Multiplication=",a*b
     elif(opp =="/"):
-        return a /b
+        return "Division=",a /b
     elif(opp =="%"):
-        return a%b
+        return "Modulus=",a%b
+    elif(opp == "//"):
+        return "Floor Division=",a//b
     else:
         return "Error"
+    
+sum=Calculator(4,3,"//")
+print(sum)
